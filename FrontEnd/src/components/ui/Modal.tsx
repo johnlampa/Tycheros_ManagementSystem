2c79c39d00754aed7e73ae35686dd1c4d3926f39
Aaron Daniel Ramirez <adgramirez@addu.edu.ph>
2024-09-30 14:22:12 +0800

    "Updated styles and layout for FoodMenu page, QuantityModal, MenuHeaderSection, MenuCard, and Modal components."

--- FrontEnd/src/components/ui/Modal.tsx
+++ FrontEnd/src/components/ui/Modal.tsx
@@ -8,21 +8,17 @@ const Modal: React.FC<ModalProps> = ({
   return (
     <>
       {modalIsVisible && (
-        <div className="fixed inset-0 z-50 flex justify-center items-center w-full">
+        <div className="fixed inset-0 z-50 flex justify-center items-center w-full h-full">
+          {/* Background Overlay */}
           <div
-            onClick={() => {
-              setModalVisibility(false);
-            }}
-            className="fixed inset-0 bg-black/20"
+            onClick={() => setModalVisibility(false)}
+            className="absolute inset-0 bg-black/20"
           ></div>
+
+          {/* Modal Content */}
           <div
             onClick={(e) => e.stopPropagation()}
-            className={`
-              z-50 bg-white rounded-xl shadow p-6 pb-32 transition-all
-              ${
-                modalIsVisible ? "scale-100 opacity-100" : "scale-125 opacity-0"
-              }
-            `}
+            className="relative bg-white rounded-xl shadow-lg p-0 m-0" // Removed all padding/margin
           >
             {children}
           </div>
